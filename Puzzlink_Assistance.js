f2612da688fec3f5a89c625eaf21bd7cd97aa093
LeavingLeaves <1141195084@qq.com>
2023-10-11 00:35:49 +0800

    Add Yajirin Assistance

--- Puzzlink_Assistance.js
+++ Puzzlink_Assistance.js
@@ -3,7 +3,7 @@
 // @version      1.0
 // @description  Do trivial deduction.
 // @author       Leaving Leaves
-// @match        https://puzz.link/p?*slither*/*
+// @match        https://puzz.link/p?*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // ==/UserScript==
@@ -11,13 +11,11 @@
 (function () {
     'use strict';
 
-    const maxLoop=20;
+    const maxLoop = 20;
 
-    var btn = '<button type="button" class="btn" id="assist" style="display: inline;">Assist</button>';
+    let btn = '<button type="button" class="btn" id="assist" style="display: inline;">Assist</button>';
     document.querySelector('#btntrial').insertAdjacentHTML('afterend', btn);
-
     document.querySelector("#assist").addEventListener("click", assist, false);
-
     window.addEventListener("keypress", (event) => {
         if (event.key !== 'q') { return; }
         assist();
@@ -25,6 +23,135 @@
 
     function assist() {
         if (/slither/.test(document.URL)) { SlitherlinkAssist(); }
+        if (/yajirin/.test(document.URL)) { YajirinAssist(); }
+        console.log('Assisted.');
+    }
+
+    let fourside = function (a, b) {
+        a(b.top);
+        a(b.bottom);
+        a(b.left);
+        a(b.right);
+    };
+
+    let fourside2 = function (a, b, c) {
+        a(b.top, c.top);
+        a(b.bottom, c.bottom);
+        a(b.left, c.left);
+        a(b.right, c.right);
+    };
+
+    function YajirinAssist() {
+        let board = ui.puzzle.board;
+        let cell = board.cell;
+        let flg = true;
+        let isEmpty = function (c) { return c.id !== null && c.qnum === -1 && c.qans === 0; };
+        for (let loop = 0; loop < maxLoop; loop++) {
+            if (!flg) { break; }
+            flg = false;
+            for (let i = 0; i < cell.length; i++) {
+                if (cell[i].qnum !== -1 && cell[i].qdir !== 0) {
+                    let qnum = cell[i].qnum;
+                    let qdir = cell[i].qdir;
+                    let dirs = [[],[-1, 0], [1, 0], [0, -1], [0, 1]];//Top Bottom Left Right
+                    let rows = board.rows, cols = board.cols;
+                    let x = Math.floor(i / cols);
+                    let y = i % cols;
+                    let emptynum = 0;
+                    let blocknum = 0;
+                    while (x >= 0 && y >= 0 && x < rows && y < cols) {
+                        emptynum += isEmpty(cell[x * cols + y]) && cell[x * cols + y].qsub!==1;
+                        blocknum += cell[x * cols + y].qans === 1;
+                        x += dirs[qdir][0];
+                        y += dirs[qdir][1];
+                    }
+                    if (emptynum + blocknum === qnum) {
+                        x = Math.floor(i / cols);
+                        y = i % cols;
+                        while (x >= 0 && y >= 0 && x < rows && y < cols) {
+                            if (isEmpty(cell[x * cols + y]) && cell[x * cols + y].qsub!==1) {
+                                flg = 1;
+                                cell[x * cols + y].setQans(1);
+                                cell[x * cols + y].draw();
+                            }
+                            x += dirs[qdir][0];
+                            y += dirs[qdir][1];
+                        }
+                    }
+                    if (blocknum === qnum){
+                        x = Math.floor(i / cols);
+                        y = i % cols;
+                        while (x >= 0 && y >= 0 && x < rows && y < cols) {
+                            if (isEmpty(cell[x * cols + y]) && cell[x * cols + y].qsub!==1) {
+                                flg = 1;
+                                cell[x * cols + y].setQsub(1);
+                                cell[x * cols + y].draw();
+                            }
+                            x += dirs[qdir][0];
+                            y += dirs[qdir][1];
+                        }
+                    }
+                }
+                if (cell[i].qnum !== -1) {
+                    continue;
+                }
+                let emptynum = 0;
+                let linenum = 0;
+                let adj = cell[i].adjacent;
+                let adb = cell[i].adjborder;
+                if (cell[i].qans === 1) {
+                    let fn = function (c) {
+                        if (!isEmpty(c)) { return; }
+                        flg |= (c.qsub !== 1);
+                        c.setQsub(1);
+                        c.draw();
+                    };
+                    fourside(fn, adj);
+                    continue;
+                }
+                let fn = function (c, d) {
+                    if (isEmpty(c) && d.qsub !== 2) { emptynum++; }
+                    linenum += d.line == 1;
+                };
+                fourside2(fn, adj, adb);
+                if (linenum === 2) {
+                    let fn = function (d) {
+                        if (d.line !== 1) {
+                            flg |= d.qsub != 2;
+                            d.setQsub(2);
+                            d.draw();
+                        }
+                    }
+                    fourside(fn, adb);
+                }
+                if (emptynum === 1) {
+                    flg |= cell[i].qans !== 1;
+                    cell[i].setQans(1);
+                    cell[i].draw();
+                }
+                if (emptynum === 2) {
+                    if (cell[i].qsub === 1 || linenum === 1) {
+                        let fn = function (c, d) {
+                            if (!isEmpty(c) || d.qsub === 2) { return; }
+                            flg |= (c.qsub !== 1) | (d.line !== 1);
+                            c.setQsub(1);
+                            d.setLine(1);
+                            c.draw();
+                            d.draw();
+                        };
+                        fourside2(fn, adj, adb);
+                    } else {
+                        let fn = function (c, d) {
+                            if (!isEmpty(c) || d.qsub === 2) { return; }
+                            flg |= (c.qsub !== 1);
+                            c.setQsub(1);
+                            c.draw();
+                        };
+                        fourside2(fn, adj, adb);
+                    }
+                }
+            }
+        }
     }
 
     function SlitherlinkAssist() {
@@ -44,12 +171,6 @@
             c.setQsub(2);
             c.draw();
         };
-        let fourside = function (a, b) {
-            a(b.top);
-            a(b.bottom);
-            a(b.left);
-            a(b.right);
-        };
         for (let loop = 0; loop < maxLoop; loop++) {
             if (!flg) { break; }
             flg = false;
@@ -60,9 +181,9 @@
                 if (cell[i].qnum === 0) {
                     fourside(addcross, adj);
                 }
-                let fn = function (c) { 
-                    if (c.qsub === 0) { emptynum++; } 
-                    linenum += (c.line == 1); 
+                let fn = function (c) {
+                    if (c.qsub === 0) { emptynum++; }
+                    linenum += (c.line == 1);
                 };
                 fourside(fn, adj);
                 if (emptynum === cell[i].qnum) {
@@ -105,29 +226,31 @@
                 let adj = cross[i].adjborder;
                 let emptynum = 0;
                 let linenum = 0;
-                let fn = function (c) { if (c !== undefined && c.id !== null && c.qsub === 0) { emptynum++; } linenum += (c.line == 1); };
+                let fn = function (c) {
+                    if (c !== undefined && c.id !== null && c.qsub === 0) { emptynum++; }
+                    linenum += (c.line == 1);
+                };
                 fourside(fn, adj);
                 if (emptynum === 1 || linenum === 2) {
                     fourside(addcross, adj);
-                } else
-                    if (emptynum === 2 && linenum === 1) {
-                        fourside(addline, adj);
-                    } else
-                        if (emptynum === 2 && linenum === 0) {
-                            let fn = function (c) { return c !== undefined && c.id !== null && c.qsub === 0; }
-                            if (fn(adj.top) && fn(adj.left) && adj.top.sidecell[0].qnum === 3) { fourside(addline, adj); }
-                            if (fn(adj.top) && fn(adj.right) && adj.top.sidecell[1].qnum === 3) { fourside(addline, adj); }
-                            if (fn(adj.bottom) && fn(adj.left) && adj.bottom.sidecell[0].qnum === 3) { fourside(addline, adj); }
-                            if (fn(adj.bottom) && fn(adj.right) && adj.bottom.sidecell[1].qnum === 3) { fourside(addline, adj); }
+                }
+                if (emptynum === 2 && linenum === 1) {
+                    fourside(addline, adj);
+                }
+                if (emptynum === 2 && linenum === 0) {
+                    let fn = function (c) { return c !== undefined && c.id !== null && c.qsub === 0; }
+                    if (fn(adj.top) && fn(adj.left) && adj.top.sidecell[0].qnum === 3) { fourside(addline, adj); }
+                    if (fn(adj.top) && fn(adj.right) && adj.top.sidecell[1].qnum === 3) { fourside(addline, adj); }
+                    if (fn(adj.bottom) && fn(adj.left) && adj.bottom.sidecell[0].qnum === 3) { fourside(addline, adj); }
+                    if (fn(adj.bottom) && fn(adj.right) && adj.bottom.sidecell[1].qnum === 3) { fourside(addline, adj); }
 
-                            if (fn(adj.top) && fn(adj.left) && adj.top.sidecell[0].qnum === 1) { fourside(addcross, adj); }
-                            if (fn(adj.top) && fn(adj.right) && adj.top.sidecell[1].qnum === 1) { fourside(addcross, adj); }
-                            if (fn(adj.bottom) && fn(adj.left) && adj.bottom.sidecell[0].qnum === 1) { fourside(addcross, adj); }
-                            if (fn(adj.bottom) && fn(adj.right) && adj.bottom.sidecell[1].qnum === 1) { fourside(addcross, adj); }
-                        }
+                    if (fn(adj.top) && fn(adj.left) && adj.top.sidecell[0].qnum === 1) { fourside(addcross, adj); }
+                    if (fn(adj.top) && fn(adj.right) && adj.top.sidecell[1].qnum === 1) { fourside(addcross, adj); }
+                    if (fn(adj.bottom) && fn(adj.left) && adj.bottom.sidecell[0].qnum === 1) { fourside(addcross, adj); }
+                    if (fn(adj.bottom) && fn(adj.right) && adj.bottom.sidecell[1].qnum === 1) { fourside(addcross, adj); }
+                }
             }
         }
-        console.log('Assisted.');
     }
 
 })();
--- README.md
+++ README.md
@@ -3,5 +3,8 @@ Help do some trivial deduction for some puzzle genre in puzz.link
 
 Press "Assist" at the bottom right or Press Q to automatically deduce.
 
+Greasyfork link:
+https://greasyfork.org/en/scripts/477117-puzz-link-assistance
+
 Currently support:
 * Slither Link
