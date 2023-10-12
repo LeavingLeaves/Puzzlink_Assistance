2a651da9c6cd974a8873854964c3a301d6c53318
LeavingLeaves <1141195084@qq.com>
2023-10-12 10:23:21 +0800

    update slitherlink and masyu

--- Puzzlink_Assistance.js
+++ Puzzlink_Assistance.js
@@ -3,8 +3,8 @@
 // @version      1.0
 // @description  Do trivial deduction.
 // @author       Leaving Leaves
-// @match        https://puzz.link/p*/*/*/*
-// @match        https://pzplus.tck.mn/p*/*/*/*
+// @match        https://puzz.link/p*/*
+// @match        https://pzplus.tck.mn/p*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // ==/UserScript==
@@ -12,7 +12,7 @@
 (function () {
     'use strict';
 
-    const maxLoop = 20;
+    const maxLoop = 30;
     let flg = true;
 
     let btn = '<button type="button" class="btn" id="assist" style="display: inline;">Assist</button>';
@@ -144,20 +144,13 @@
                             add_cross(dir(adjline, d + 1));
                             add_cross(dir(adjline, d + 3));
                         }
-                        //3 in a row
-                        if (dir(adjcell, d).qnum === 2 && dir(adjcell, d + 2).qnum === 2) {
-                            add_line(dir(adjline, d + 1));
-                            add_line(dir(adjline, d + 3));
-                            add_cross(dir(adjline, d));
-                            add_cross(dir(adjline, d + 2));
-                        }
                         //turn at one side
                         if (dir(adjline, d).line === 1 && dir(dir(adjcell, d).adjborder, d).line === 1) {
                             add_cross(dir(dir(adjcell, d + 2).adjborder, d + 2));
                         }
                         //no turn on both side
-                        if (!dir(adjcell, d).isnull && dir(dir(adjcell, d).adjborder, d).line === 1 &&
-                            !dir(adjcell, d + 2).isnull && dir(dir(adjcell, d + 2).adjborder, d + 2).line === 1) {
+                        if ((!dir(adjcell, d).isnull && dir(dir(adjcell, d).adjborder, d).line === 1 || dir(adjcell, d).qnum === 1) &&
+                            (!dir(adjcell, d + 2).isnull && dir(dir(adjcell, d + 2).adjborder, d + 2).line === 1 || dir(adjcell, d + 2).qnum === 1)) {
                             add_line(dir(adjline, d + 1));
                             add_line(dir(adjline, d + 3));
                             add_cross(dir(adjline, d));
@@ -170,7 +163,7 @@
                         //can't go straight this way
                         if (dir(adjcell, d).isnull || dir(adjline, d).qsub === 2 ||
                             dir(dir(adjcell, d).adjacent, d).isnull || dir(dir(adjcell, d).adjborder, d).qsub === 2 ||
-                            dir(adjcell, d).qnum === 2) {
+                            dir(adjcell, d).qnum === 2 || dir(adjline, d + 2).line === 1) {
                             add_cross(dir(adjline, d));
                             add_line(dir(adjline, d + 2));
                         }
@@ -422,6 +415,45 @@
                     if (fn(adjline.bottom) && fn(adjline.left) && adjline.bottom.sidecell[0].qnum === 1) { fourside(add_cross, adjline); }
                     if (fn(adjline.bottom) && fn(adjline.right) && adjline.bottom.sidecell[1].qnum === 1) { fourside(add_cross, adjline); }
                 }
+                //2 degree turn or line enter
+                {
+                    let fn = function (c1, c2, c3, c4, c5, c6, c34) {
+                        if (c34.isnull) { return; }
+                        //avoid 1*1 loop with 2 degree turn
+                        if ((c1.isnull || c1.qsub === 2) && (c2.isnull || c2.qsub === 2) && c5.line === 1 && c6.line === 1) {
+                            add_cross(c3);
+                            add_cross(c4);
+                        }
+                        //line enters 1
+                        if (!c1.isnull && c1.line === 1 && (c2.isnull || c2.qsub === 2) && c34.qnum === 1) {
+                            add_cross(c5);
+                            add_cross(c6);
+                        }
+                        //2 degree turn with 2
+                        if ((c1.isnull || c1.qsub === 2) && (c2.isnull || c2.qsub === 2) && (c5.qsub === 2 || c6.qsub === 2) && c34.qnum === 2) {
+                            add_line(c3);
+                            add_line(c4);
+                        }
+                        //line enters 3
+                        if (!c1.isnull && c1.line === 1 && c34.qnum === 3) {
+                            add_cross(c2);
+                            add_line(c5);
+                            add_line(c6);
+                        }
+                    };
+                    if (!crsline.bottom.isnull) {
+                        fn(crsline.top, crsline.left, crsline.bottom, crsline.right, crsline.bottom.sidecell[1].adjborder.bottom, crsline.bottom.sidecell[1].adjborder.right, crsline.bottom.sidecell[1]);
+                        fn(crsline.left, crsline.top, crsline.bottom, crsline.right, crsline.bottom.sidecell[1].adjborder.bottom, crsline.bottom.sidecell[1].adjborder.right, crsline.bottom.sidecell[1]);
+                        fn(crsline.top, crsline.right, crsline.bottom, crsline.left, crsline.bottom.sidecell[0].adjborder.bottom, crsline.bottom.sidecell[0].adjborder.left, crsline.bottom.sidecell[0]);
+                        fn(crsline.right, crsline.top, crsline.bottom, crsline.left, crsline.bottom.sidecell[0].adjborder.bottom, crsline.bottom.sidecell[0].adjborder.left, crsline.bottom.sidecell[0]);
+                    }
+                    if (!crsline.top.isnull) {
+                        fn(crsline.bottom, crsline.left, crsline.top, crsline.right, crsline.top.sidecell[1].adjborder.top, crsline.top.sidecell[1].adjborder.right, crsline.top.sidecell[1]);
+                        fn(crsline.left, crsline.bottom, crsline.top, crsline.right, crsline.top.sidecell[1].adjborder.top, crsline.top.sidecell[1].adjborder.right, crsline.top.sidecell[1]);
+                        fn(crsline.bottom, crsline.right, crsline.top, crsline.left, crsline.top.sidecell[0].adjborder.top, crsline.top.sidecell[0].adjborder.left, crsline.top.sidecell[0]);
+                        fn(crsline.right, crsline.bottom, crsline.top, crsline.left, crsline.top.sidecell[0].adjborder.top, crsline.top.sidecell[0].adjborder.left, crsline.top.sidecell[0]);
+                    }
+                }
             }
             //avoid forming multiple loop
             for (let i = 0; i < border.length; i++) {
@@ -496,6 +528,18 @@
                     }
                     if (cell[i].qnum === 2 && innernum === 2) {
                         fourside(add_bg_outer_color, adjcell);
+                    }
+                    //2 different color around 1 or 3
+                    {
+                        let fn = function (c, d) {
+                            if (!c.isnull && c.qsub === 0) {
+                                if (cell[i].qnum === 1) { add_cross(d); }
+                                if (cell[i].qnum === 3) { add_line(d); }
+                            }
+                        };
+                        if ((cell[i].qnum === 1 || cell[i].qnum === 3) && innernum === 1 && outernum === 1) {
+                            fourside2(fn, adjcell, adjline);
+                        }
                     }
                 }
                 //checker pattern
