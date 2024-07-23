4d0006ddc89b5c8618ab6257ba4b93651255f245
LeavingLeaves <1141195084@qq.com>
2024-07-23 23:01:00 +0800

    version

--- Puzzlink_Assistance.js
+++ Puzzlink_Assistance.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Puzz.link Assistance
-// @version      24.7.21.1
+// @version      24.7.23.1
 // @description  Do trivial deduction.
 // @author       Leaving Leaves
 // @match        https://puzz.link/p*/*
